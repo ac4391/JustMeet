729e13e2b7db4ed23501b2f58b56e22ea22cbd16
Marko Mandic <marko@dyn-160-39-230-69.dyn.columbia.edu>
2019-04-08 10:49:15 -0400

    list applicants

---
+++ screens/ApplicantsScreen.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { FlatList, ActivityIndicator, Text, View, StyleSheet, Linking, ScrollView  } from 'react-native';
+import { Icon } from 'expo';
+import Amplify, { Auth, API, graphqlOperation } from 'aws-amplify';
+import { listApplicants } from '../src/graphql/queries';
+
+
+export default class ApplicantsScreen extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true,
+                  applicants: []}
+  }
+
+  componentDidMount(){
+      this.listApplicants()
+      this.state.isLoading = false
+  }
+
+  listApplicants = async () => {
+    try {
+      const graphqldata = await API.graphql(
+        graphqlOperation(listApplicants)
+      )
+      this.setState(
+        {
+          applicants: graphqldata.data.listApplicants.items,
+          // reset the input field to empty after post creation
+        }
+      )
+    }
+    catch (err) {
+      console.log('error: ', err)
+    }
+  }
+
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+      return (
+    <View style={{flex: 1}}>
+      <ScrollView>
+        <View style={{flex: 1}}>
+          {
+            this.state.applicants.map((applicant, index) => (
+              <View style={{flexDirection: 'row'}}>
+                <Icon.Ionicons onPress={ ()=> Linking.openURL('https://www.linkedin.com/in/ariel-cohen-codar-301020141/') } name={'logo-linkedin'} size={20} color={'#4875B4'}/><Text>{applicant.username}</Text>
+              </View>
+            ))
+          }
+        </View>
+      </ScrollView>
+    </View>
+  )
+
+  }
+}
